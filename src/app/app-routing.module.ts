ad4b7d4a7c8b0ecd78820b189fd64e25c12be5b7
matirue <matiasrue@gmail.com>
2021-08-27 18:11:11 -0300

    Primer script

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { HomeComponent } from './component/home/home.component';
+import { QuienSoyComponent } from './component/quien-soy/quien-soy.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path:'', component: LoginComponent },
+  { path:'home', component: HomeComponent },
+  { path:'quienSoy', component: QuienSoyComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { HomeComponent } from './component/home/home.component';
+import { QuienSoyComponent } from './component/quien-soy/quien-soy.component';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    QuienSoyComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
