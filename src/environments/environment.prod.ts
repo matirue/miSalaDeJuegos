cdb1a22be6aaa4bbf63ab6c380685f469e3e746f
matirue <matiasrue@gmail.com>
2021-09-09 03:21:08 -0300

    arreglo environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB8awtCsDhNNdZuVD67kvSV-467Lw-pP8Q",
+    authDomain: "saladejuego-637cd.firebaseapp.com",
+    projectId: "saladejuego-637cd",
+    storageBucket: "saladejuego-637cd.appspot.com",
+    messagingSenderId: "300891569789",
+    appId: "1:300891569789:web:477022e64104905185bb14",
+    measurementId: "G-N31RQ62EF6"
+  }
 };
